57ece08099f0f9b797f9ea2f37935dfd25517925
Michal Stachnik <stachnikmichal@gmail.com>
2020-01-19 14:04:49 -0500

    Added back button to poll page

--- client/src/pages/Poll/Poll.tsx
+++ client/src/pages/Poll/Poll.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Chart from 'react-apexcharts';
 
 import { UserContext } from '../../contexts/UserContext';
@@ -105,33 +106,38 @@ class Poll extends React.Component<Props, State> {
   render() {
     if (!this.state.poll) return <h1>Loading...</h1>;
     return (
-      <div className="card text-white bg-primary my-3">
-        <div className="card-header">{this.state.poll.question}</div>
-        <div className="card-body">
-          <div className="card-body-left">
-            {/* <h4 className="card-title">lello</h4> */}
-            {this.state.poll.options?.map((option: any, index: number) => {
-              return (
-                <div className="option mb-3">
-                  <p className="card-text" key={index}>
-                    {option.voteCount} {option.option}
-                  </p>
-                  <button
-                    type="button"
-                    className="btn btn-outline-info"
-                    onClick={() => this.handleVoteClick(option.option)}
-                  >
-                    Vote
-                  </button>
-                </div>
-              );
-            })}
+      <div>
+        <button type="button" className="btn btn-outline-secondary mt-3">
+          <Link to="/">Back</Link>
+        </button>
+        <div className="card text-white bg-primary my-3">
+          <div className="card-header">{this.state.poll.question}</div>
+          <div className="card-body">
+            <div className="card-body-left">
+              {/* <h4 className="card-title">lello</h4> */}
+              {this.state.poll.options?.map((option: any, index: number) => {
+                return (
+                  <div className="option mb-3">
+                    <p className="card-text" key={index}>
+                      {option.voteCount} {option.option}
+                    </p>
+                    <button
+                      type="button"
+                      className="btn btn-outline-info"
+                      onClick={() => this.handleVoteClick(option.option)}
+                    >
+                      Vote
+                    </button>
+                  </div>
+                );
+              })}
+            </div>
+            <Chart
+              options={this.state.donutOptions.options}
+              series={this.state.donutOptions.series}
+              type="donut"
+            />
           </div>
-          <Chart
-            options={this.state.donutOptions.options}
-            series={this.state.donutOptions.series}
-            type="donut"
-          />
         </div>
       </div>
     );
